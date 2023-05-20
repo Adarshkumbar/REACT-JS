1815575dfef2cc86c4a5dfd2269d54121ba65917
Adarsh Kumbar <122893949+Adarshkumbar@users.noreply.github.com>
2023-05-20 12:59:35 +0530

    Update 05-useState-gotcha.js

--- src/5_useState Hook/tutorial/01-useState/starter/05-useState-gotcha.js
+++ src/5_useState Hook/tutorial/01-useState/starter/05-useState-gotcha.js
@@ -6,7 +6,11 @@ const UseStateGotcha = () => {
   const handleClick = () =>{
     setValue(value+1)   //this is ahead than below 
     console.log(value);
-  } 
+    // what we can do is
+//     setValue(()=>{
+//       return x+1
+//     })
+//     } 
   return (
     <>
       <h1>useState "gotcha"</h1>
