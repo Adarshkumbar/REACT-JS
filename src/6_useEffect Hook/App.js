4359d6d4e3fdbaecac853bdd54b6aa54bbe42e4b
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-05-21 23:57:39 +0530

    useEffect Hook

---
+++ src/6_useEffect Hook/App.js
@@ -0,0 +1,22 @@
+//****************** FOR useState ***********************
+
+// import Starter from "./tutorial/01-useState/starter/02-useState-basics"
+// import Starter from "./tutorial/01-useState/starter/04-useState-object";
+// import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
+
+//****************** FOR useEffect ***********************
+// import Starter from './tutorial/02-useEffect/starter/01-code-example'
+// import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics'
+// import Starter from './tutorial/02-useEffect/starter/03-multiple-effects'
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Starter/>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/6_useEffect Hook/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' 
+import './index.css'
+
+import Starter from './tutorial/01-useState/starter/03-useState-array.jsx'
+import Final from "./tutorial/01-useState/final/03-useState-array.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    {/* <Starter /> */}
+    <App />
+  </>
+);
